b8479da0b99fb23323223012f828414cabd67472
Stanimir Stoyanov <stan@dev.bildau.de>
2023-01-24 19:12:37 +0200

    fix: adding more fun to the exercise :)

--- cli/random_name.php
+++ cli/random_name.php
@@ -1,8 +1,10 @@
 <?php
 
 /**
- * This script should print  "Here is the name: $name - $name2"
+ * 1) This script should print  "Here is the name: $name - $name2"
  * $name variables are decided as seen in the code, fix all the bugs whilst keeping the functionality!
+ *
+ * 2) The print_r should print 5 random names; Please modify only the functions themselves, not the way we invoke them.
  */
 
 
@@ -35,3 +37,9 @@ function randomHeroName()
 }
 
 echo "Here is the name: " . combineNames();
+
+echo PHP_EOL . PHP_EOL;
+
+$fiveHeroesHolder = [];
+randomGenerate($fiveHeroesHolder, 5);
+print_r($fiveHeroesHolder);
