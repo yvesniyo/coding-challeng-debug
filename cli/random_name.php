6d132b1a03e7f65befc7fdac06da959b154ddec8
yvesniyo <niyobuhungiro.yves@gmail.com>
2023-12-13 19:11:08 +0200

    fixed the random name issue

--- cli/random_name.php
+++ cli/random_name.php
@@ -11,16 +11,18 @@
 $arr = [];
 function combineNames($str1 = "", $str2 = "")
 {
+
 	$params = [$str1, $str2];
-	foreach ($params as $param) {
+	foreach ($params as $index => $param) {
+
 		if ($param == "") {
-			$param = randomHeroName();
+			$params[$index] = randomHeroName();
 		}
 	}
-	echo implode($params, " - ");
+	echo implode(" - ", $params);
 }
 
-function randomGenerate($arr, $amount)
+function randomGenerate(&$arr, $amount)
 {
 	for ($i = $amount; $i > 0; $i--) {
 		array_push($arr, randomHeroName());
@@ -31,9 +33,10 @@ function randomGenerate($arr, $amount)
 function randomHeroName()
 {
 	$hero_firstnames = ["captain", "doctor", "iron", "Hank", "ant", "Wasp", "the", "Hawk", "Spider", "Black"];
-	$hero_lastnames = ["America", "Strange", "man", "Pym", "girl", "hulk", "eye", "widow", "panther", "daredevil"]
-	$heroes = [$hero_firstnames, $hero_lastnames];
-	echo $heroes[rand(0, count($heroes))][rand(0, 10)];
+	$hero_lastnames = ["America", "Strange", "man", "Pym", "girl", "hulk", "eye", "widow", "panther", "daredevil"];
+	shuffle($hero_firstnames);
+	shuffle($hero_lastnames);
+	return $hero_firstnames[0] . " " . $hero_lastnames[0];
 }
 
 echo "Here is the name: " . combineNames();
